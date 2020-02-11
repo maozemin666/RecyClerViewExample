0115c49c78b12f69c95a2b6b3dd75df7d57267c3
maozemin <1641762424@qq.com>
2020-02-11 13:03:47 +0800

    kotlin

--- app/src/main/java/com/example/recyclerviewexample/activity/KotlinActivity.kt
+++ app/src/main/java/com/example/recyclerviewexample/activity/KotlinActivity.kt
@@ -3,23 +3,22 @@ package com.example.recyclerviewexample.activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.recyclerviewexample.R
+import com.example.recyclerviewexample.kotlin.KotlinDemo
 
 class KotlinActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_kotlin)
-    }
-
-    override fun initView() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
-
-    override fun init() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 
     override fun getLayoutId(): Int {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return R.layout.activity_kotlin
+    }
+
+    override fun initView() {
+    }
+
+    override fun init() {
+        var kotlinDemo = KotlinDemo()
     }
 }
---
+++ app/src/main/java/com/example/recyclerviewexample/kotlin/KotlinDemo.kt
@@ -0,0 +1,6 @@
+package com.example.recyclerviewexample.kotlin
+
+class KotlinDemo {
+
+
+}
