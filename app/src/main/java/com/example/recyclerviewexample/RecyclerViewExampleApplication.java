0e088faf8e04f83a80293d7e9d0a0ebbf79d1f26
maozemin <1641762424@qq.com>
2020-03-23 21:52:51 +0800

    update gradle

--- app/src/main/java/com/example/recyclerviewexample/RecyclerViewExampleApplication.java
+++ app/src/main/java/com/example/recyclerviewexample/RecyclerViewExampleApplication.java
@@ -2,8 +2,6 @@ package com.example.recyclerviewexample;
 
 import android.app.Application;
 
-import com.xuexiang.xui.XUI;
-
 
 public class RecyclerViewExampleApplication extends Application {
 
@@ -11,7 +9,5 @@ public class RecyclerViewExampleApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-        XUI.init(this); //初始化UI框架
-        XUI.debug(true);  //开启UI框架调试日志
     }
 }
